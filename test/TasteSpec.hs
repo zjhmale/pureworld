6ec72b51d15b7ec9638ed03af914d383399c13b7
zjhmale <zjhmale@gmail.com>
2015-10-22 09:08:28 +0800

    add firststep

--- test/TasteSpec.hs
+++ test/TasteSpec.hs
@@ -5,5 +5,9 @@ import Test.Hspec
 spec :: Spec
 spec = do
     describe "sum a num list" $ do
-        it "encodes multiples of 3" $
-            sum' [1,2,3] `shouldBe` (6 :: Int)
+      it "encodes multiples of 3" $
+        sum' [1,2,3] `shouldBe` (6 :: Int)
+
+    describe "quick sort a num list" $ do
+      it "quick sort [1,5,4,2,3]" $
+        qsort [1,5,4,2,3] `shouldBe` ([1,2,3,4,5] :: [Int])
