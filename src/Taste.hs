ab1d6ac3d47def42e571bb43a3f29c670c656cde
zjhmale <zjhmale@gmail.com>
2015-10-22 20:55:51 +0800

    try to be simple

--- src/Taste.hs
+++ src/Taste.hs
@@ -2,6 +2,7 @@
 --module Taste (add) where
 --haskell的module机制是如果像上面一样只标明add那么这个module就只export add这个function 其他都对外部module不可见
 module Taste where
+import Data.List
 
 sum' :: Num a => [a] -> a
 sum' [] = 0
@@ -14,4 +15,35 @@ qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
                     smaller = [a | a <- xs, a <= x]
                     larger = [b | b <- xs, b > x]
 
+-- single line comment
+
+{-
+multiple
+lines
+comments
+-}
+
+double :: Num n => n -> n
+double x = x + x
+
+quadruple :: Num n => n -> n
+quadruple x = double $ double x
+
+factorial :: Int -> Int
+factorial n = product [1..n]
+
+average :: Fractional a => [a] -> a
+average ns = sum ns / genericLength ns
+
+implicit :: Int
+implicit = b + c
+           where
+             b = 1
+             c = 2
+
+explicit :: Int
+explicit = b + c
+           where
+             {b = 1;
+              c = 2}
 
--- test/TasteSpec.hs
+++ test/TasteSpec.hs
@@ -11,3 +11,44 @@ spec = do
     describe "quick sort a num list" $ do
       it "quick sort [1,5,4,2,3]" $
         qsort [1,5,4,2,3] `shouldBe` ([1,2,3,4,5] :: [Int])
+
+
+    describe "num operation" $ do
+      it "double a num" $
+        double 3 `shouldBe` (6 :: Int)
+      it "quadruple a num" $
+        quadruple 3 `shouldBe` (12 :: Int)
+      it "factorial a num" $
+        factorial 10 `shouldBe` (3628800 :: Int)
+      it "average a int list" $
+        average [1,2,3,4,5] `shouldBe` (3.0 :: Float)
+      it "average a float list" $
+        average [1.0,2.0,3.0,4.0,5.0] `shouldBe` (3.0 :: Float)
+
+    describe "list operation" $ do
+      it "get head of a list" $
+        head [1,2,3,4,5] `shouldBe` (1 :: Int)
+      it "get tail of a list" $
+        tail [1,2,3,4,5] `shouldBe` ([2,3,4,5] :: [Int])
+      it "get nth element of a list" $
+        [1,2,3,4,5] !! 2 `shouldBe` (3 :: Int)
+      it "select first n elements of a list" $
+        take 3 [1,2,3,4,5] `shouldBe` ([1,2,3] :: [Int])
+      it "remove first n elements of a list" $
+        drop 3 [1,2,3,4,5] `shouldBe` ([4,5] :: [Int])
+      it "length of a list" $
+        length ([1,2,3,4,5] :: [Int]) `shouldBe` (5 :: Int)
+      it "sum of a list" $
+        sum [1,2,3,4,5] `shouldBe` (15 :: Int)
+      it "product of a list" $
+        product [1,2,3,4,5] `shouldBe` (120 :: Int)
+      it "append two lists" $
+        [1,2,3] ++ [4,5] `shouldBe` ([1,2,3,4,5] :: [Int])
+      it "reverse a list" $
+        reverse [1,2,3,4,5] `shouldBe` ([5,4,3,2,1] :: [Int])
+
+    describe "layout rule" $ do
+      it "use implicit syntax" $
+        implicit * 2 `shouldBe` (6 :: Int)
+      it "use explicit syntax" $
+        explicit * 2 `shouldBe` (6 :: Int)
